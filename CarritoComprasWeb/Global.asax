e078b60fd7f3aaa5bbf98804000cef4e5e2ea3b2
SabrinaFranchi <sabrina_franchi@hotmail.com>
2018-10-18 22:10:31 -0300

    Inicio

---
+++ CarritoComprasWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="CarritoComprasWeb.Global_asax" Language="vb" %>
---
+++ Repositorio/Conexion.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Conexion
+    Dim con As SqlConnection
+    Public puntero As SqlConnection
+    Public EstadoConexion As Boolean = False
+    Public Sub conectar()
+        con = New SqlConnection
+        con.ConnectionString = ConfigurationManager.ConnectionStrings("BDSegMascConnectionString").ToString
+        Try
+            con.Open()
+            puntero = con
+            EstadoConexion = True
+        Catch ex As Exception
+            EstadoConexion = False
+        End Try
+    End Sub
+    Public Sub desconectar()
+        con.Close()
+    End Sub
+End Class
